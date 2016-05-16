9da540368f78a65c64183059c757237f8baf7b10
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-05-16 17:44:44 +0200

    add cassette for prepared test

--- tests/test_ais.py
+++ tests/test_ais.py
@@ -74,14 +74,15 @@ class TestAIS(unittest.TestCase):
         with open(fixture_path('expected_digest')) as fp:
             self.assertEqual(fp.read(), actual_digest)
 
-    def test_sign_pdf(self):
+    def test_sign_prepared_pdf(self):
         """Test signature of a single file.
 
         Given I am authenticated
         When a sign an existing file
         Then I receive a signature
         """
-        self.instance.sign_pdf(filename=fixture_path('prepared.pdf'))
+        with my_vcr.use_cassette('sign_prepared_pdf'):
+            self.instance.sign_pdf(filename=fixture_path('prepared.pdf'))
 
     def test_wrong_customer_auth_error(self):
         """Test an AuthenticationFailed."""
