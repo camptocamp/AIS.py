76e75f815cbd17b2097cef95360bbc8180af080f
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-05-18 11:09:21 +0200

    prepare for 0.2.0 release cycle

--- AIS/__init__.py
+++ AIS/__init__.py
@@ -14,4 +14,4 @@ from .exceptions import AISError, AuthenticationFailed
 __all__ = ('login', 'AIS', 'PDF', 'Signature', 'AISError',
            'AuthenticationFailed')
 
-__version__ = '0.1.0'
+__version__ = '0.2.0dev0'
